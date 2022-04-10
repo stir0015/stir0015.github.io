c52d835bc9e4107f2ea98fe662adb3b445507e0e
stir0015 <stir0015@algonquinlive.com>
2022-04-10 11:07:09 -0400

    Week 13

--- lib2024/final/js/scripts.js
+++ lib2024/final/js/scripts.js
@@ -2,45 +2,34 @@ window.onload = init;
 
 function init() {
 
-	// JQuery function attached to the submit event of the form with id "form"
-	$('#profile').submit(function (e) {
-		// e.preventDefualt to avoid the form being submitted to page specified in action attribute 
-   		 e.preventDefault();
-   		 // passing the current form (this) to variable form 
-   		 $("#form-display").removeClass('reveal');
-   		 /*var form = this;
-   		 // fadeIn is a jQuery function to fadeIn an element 
-   		 $(".overlay-container").fadeIn(1000, function(){
-   		 		//call the showFormValues function and pass variable form to it as argument
-   		 		showformValues(form);
-		   		 // fadeOut is a jQuery function to fadeOut an element 
-   		 		$('.overlay-container').delay(500).fadeOut(500);
-   		 		$("#results").addClass('reveal');
-   		 })*/
-	});
+	var img = document.querySelectorAll('.image-side');
+	// we have to use for look to iterate through the elements
+	for (var i = 0; i < img.length; i++) {
+	  
+	  imgThumbs[i].addEventListener('click', showImgLightBox);
+	}
+
+	// Add onclick function to hide overlay when clicked
+	document.querySelector('#img-overlay').onclick = hideImgLightBox;
 
 }
 
-// to show the form values in the results div which takes the argument "form"
-function showformValues(form){
-	//serializeArray is a jquery function used to get the values of a form as js Object
-	var formValues = $(form).serializeArray(); 
-	// $.each is a jquery alternative to for loop to iterate through an JS array or object  (Especially beneficial when the length of array is not known)
-	// index is the index  of the current element i.e 0,1,2,3 so on 
-		//field is the actual field being accessed 
-		
-	$.each(formValues, function(index, field){
-
-		// following code does the following : 
-		// 1) $("#results") -- (Gets the  selects the div with id results 
-		// 2) .fund("#"+field.name+"_result") -- finds the element with id equal to the name of the field being accessed along with text ("_result") Eg : name, pc_result, email_result
-		// 3) Modifies the text inside the selected element and replaces it with the value of this field   
-		$("#form-display").find("#"+field.name+"_result").text(field.value);
-
-		// special check for email to add a link instead of just string
-		if(field.name=="email"){
-			$("#form-display").find("#"+field.name+"_result").attr("href", "mailto:"+field.value);
-		}
-	})				
+function showImgLightBox(){
+	//getAttribute is used to get attribute value of an element, the attribute name is passed in the ()
+	var targetImgId = this.getAttribute("data-img-id");
+	console.log(targetImgId);
+	//show overlay container
+	document.querySelector('#img-overlay').style.display ="block";
+	//#1 - remove the active class from the current active slide
+	//get element that has both the classes image-slide and active and remove the class active from it
+	document.querySelector('.img-slide.active').classList.remove('active');
+	// console.log(document.querySelector("#"+targetImgId));
+	// Add the active class to the slide attached to the "data-img-id" attribute of this thumbnail
+	document.querySelector("#"+targetImgId).classList.add('active');
 }
+
+function hideImgLightBox(){
+	document.querySelector('#img-overlay').style.display ="none";
+}
+
 
