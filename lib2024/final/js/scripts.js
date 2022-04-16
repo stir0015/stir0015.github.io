a86b600e6e7121995b6914f4455b4b2c15ca5dcd
stir0015 <stir0015@algonquinlive.com>
2022-04-16 18:22:06 -0400

    Week 14

--- lib2024/final/js/scripts.js
+++ lib2024/final/js/scripts.js
@@ -1,37 +1,33 @@
 window.onload = init;
 
 function init() {
-	document.getElementById("image-1").onclick = showImgLightBox;
-	document.getElementById("image-overlay").onclick = hideImgLightBox;
-	document.getElementById("submit").onclick = ShowFormValues
+	document.getElementById("image-1").onclick = showImgLightBox; //function to open lightbox on click
+	document.getElementById("image-overlay").onclick = hideImgLightBox; //function to close on click
 }
 
-function showImgLightBox(){
-	document.getElementById("image-overlay").style.display ="block";
+function showImgLightBox() { //function to reveal the lightbox overlay
+	document.getElementById("image-overlay").style.display = "block";
 }
 
-function hideImgLightBox(){
-	document.getElementById("image-overlay").style.display ="none";
+function hideImgLightBox() { //to hide
+	document.getElementById("image-overlay").style.display = "none";
 }
 
-function showFormValues(form){
-	//serializeArray is a jquery function used to get the values of a form as js Object
-	var formValues = $(form).serializeArray(); 
-	// $.each is a jquery alternative to for loop to iterate through an JS array or object  (Especially beneficial when the length of array is not known)
-	// index is the index  of the current element i.e 0,1,2,3 so on 
-		//field is the actual field being accessed 
-		
-	$.each(formValues, function(index, field){
+$('#profile').submit(function(e) { //form validation
+	e.preventDefault();
+	var form = this;
+	$(".buffer-container").fadeIn(1000, function() {
+		showProfileValues(form);
+		$('.buffer-container').delay(500).fadeOut(500);
+		$("#profile-info").addClass('reveal');
+	});
+});
 
-		// following code does the following : 
-		// 1) $("#results") -- (Gets the  selects the div with id results 
-		// 2) .fund("#"+field.name+"_result") -- finds the element with id equal to the name of the field being accessed along with text ("_result") Eg : name, pc_result, email_result
-		// 3) Modifies the text inside the selected element and replaces it with the value of this field   
-		$("#results").find("#"+field.name+"_result").text(field.value);
+function showProfileValues(form) {
+	var profileValues = $(profile).serializeArray();
 
-		// special check for email to add a link instead of just string
-		if(field.name=="email"){
-			$("#results").find("#"+field.name+"_result").attr("href", "mailto:"+field.value);
-		}
-	})				
+	$.each(profileValues, function(index, field) {
+
+		$("#profile-info").find("#" + field.name + "_result").text(field.value);
+	});
 }
